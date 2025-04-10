ce346b7ff158158cbaa4c1efa336f85661bad118
ramswaroop1 <ramswaroop@coedify.com>
2025-04-10 12:04:43 +0530

    AdacencyListGraph

--- graph/com/Part_1_initilize.java
+++ graph/com/Part_1_initilize.java
@@ -18,6 +18,28 @@ public class Part_1_initilize {
 		adjecentMatrix[i][j] = 0;
 		adjecentMatrix[j][i] = 0;
 	}
+	public void addVertex(int v) {
+		int newAdjcentMatrix[][] = new int[numVertices+1][numVertices + 1];
+		for(int i =0; i<numVertices; i++) {
+			for(int j =0; j<numVertices; j++) {
+				newAdjcentMatrix[i][j] = adjecentMatrix[i][j];	
+			}
+		}
+		adjecentMatrix = newAdjcentMatrix;
+		numVertices++;
+	}
+	public void removeVertex(int v) {
+		int newAdjcentMatrix[][] = new int[numVertices-1][numVertices - 1];
+		for(int i =0; i<numVertices; i++) {
+			for(int j =0; j<numVertices; j++) {
+				if(i != v && j != v) 
+				newAdjcentMatrix[i][j] = adjecentMatrix[i][j];
+				
+			}
+		}
+		adjecentMatrix = newAdjcentMatrix;
+		numVertices--;
+	}
 	public static void main(String arg[]) {
 		Part_1_initilize grpah = new Part_1_initilize(4);
 	}

d00138ad4801b212bf64e1b56b61036c66b2522a
ramswaroop1 <ramswaroop@coedify.com>
2025-04-28 19:33:27 +0530

    evaluate expression using stack

--- com/string/EvaluteExpression.java
+++ com/string/EvaluteExpression.java
@@ -40,49 +40,11 @@ public class EvaluteExpression {
 		return result;
 	}
 	
-	 public static int evaluteExpression2(String s) {
-	        Stack<Integer> stack = new Stack<>();
-	        char sign = '+';
-	        int num = 0;
-	        int n = s.length();
-	        
-	        for (int i = 0; i < n; i++) {
-	            char c = s.charAt(i);
-	            
-	     
-	            if (Character.isDigit(c)) {
-	                num = num * 10 + (c - '0');
-	            }
-	           
-	            if ((!Character.isDigit(c) && c != ' ') || i == n - 1) {
-	                if (sign == '+') {
-	                    stack.push(num); 
-	                } else if (sign == '-') {
-	                    stack.push(-num); 
-	                } else if (sign == '*') {
-	                    stack.push(stack.pop() * num);
-	                } else if (sign == '/') {
-	                    stack.push(stack.pop() / num); 
-	                }
-	                
-	                
-	                sign = c;
-	                num = 0;
-	            }
-	        }
-	        
-	        // Sum up all the values in the stack to get the final result
-	        int result = 0;
-	        for (int i : stack) {
-	            result += i;
-	        }
-	        
-	        return result;
-	    }
+	 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String string="3*2-11*3-7+5";
-		System.out.println(evaluteExpression2(string));
+		
 		System.out.println(evaluteExpression(string));
        
 	}
